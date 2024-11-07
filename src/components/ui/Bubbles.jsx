0ac30146a63c04e3cc6369455341b22cedaf368f
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-11-07 12:17:12 +0200

    feat: add bubbles component

---
+++ src/components/ui/Bubbles.jsx
@@ -0,0 +1,11 @@
+function Bubbles() {
+  return (
+    <div className="bubbles">
+    <span className="bg-purple"></span>
+    <span className="bg-purple"></span>
+    <span className="bg-purple"></span>
+  </div>
+  )
+}
+
+export default Bubbles
