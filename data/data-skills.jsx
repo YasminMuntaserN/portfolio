73c3ce92f0f7cad17d7109c24da3861cd00bc2d8
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-11-07 12:18:53 +0200

    feat: add data files

---
+++ data/data-skills.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { FaCsharp, FaHtml5, FaCss3Alt, FaGitAlt, FaReact, FaDatabase } from "react-icons/fa";
+import { SiCplusplus, SiJavascript, SiDotnet, SiMicrosoftsqlserver, SiRedux, SiStyledcomponents } from "react-icons/si";
+import { AiOutlineApi } from "react-icons/ai";
+import { FiSettings } from "react-icons/fi";
+
+export const skills = [
+  { name: ".NET", icon: <SiDotnet /> },
+  { name: "ADO.NET", icon: <SiDotnet /> },
+  { name: "REST APIs", icon: <AiOutlineApi /> },
+  { name: "React", icon: <FaReact /> },
+  { name: "Redux", icon: <SiRedux /> },
+  { name: "C#", icon: <FaCsharp /> },
+  { name: "C++", icon: <SiCplusplus /> },
+  { name: "SQL Server", icon: <SiMicrosoftsqlserver /> },
+  { name: "T-SQL", icon: <FaDatabase /> },
+  { name: "LINQ", icon: <FiSettings /> },
+  { name: "HTML", icon: <FaHtml5 /> },
+  { name: "CSS", icon: <FaCss3Alt /> },
+  { name: "JavaScript", icon: <SiJavascript /> },
+  { name: "Git", icon: <FaGitAlt /> },
+  { name: "3-Tier Architecture", icon: <FiSettings /> },
+  { name: "Windows Forms", icon: <SiDotnet /> },
+  { name: "Algorithms", icon: <FiSettings /> },
+  { name: "Data Structures", icon: <FiSettings /> },
+  { name: "Structured Programming", icon: <FiSettings /> },
+  { name: "Object-Oriented Programming", icon: <FiSettings /> },
+  { name: "SOLID Principles", icon: <FiSettings /> },
+  { name: "Problem Solving", icon: <FiSettings /> },
+  { name: "Styled components", icon: <SiStyledcomponents /> },
+
+];
---
+++ data/data-tools.js
@@ -0,0 +1,12 @@
+export const tools = [
+  ".NET Framework",
+  "SQL Server",
+  "Vite",
+  "TailwindCSS",
+  "React",
+  "Supabase",
+  "React Query",
+  "React Router",
+  "Styled Components",
+  "Leaflet",
+];
