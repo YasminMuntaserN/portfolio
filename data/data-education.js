7b5fea2fd8d917fe89857318e6ed8c04596e89ed
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-11-11 15:34:50 +0200

    feat: add education data

---
+++ data/data-education.js
@@ -0,0 +1,17 @@
+export const education = [
+  {
+    name: "University of Palestine",
+    date: "2022-present",
+    site: "https://en.up.edu.ps/",
+  },
+  {
+    name: "25 course with programming advice",
+    date: "2023-present",
+    site: "https://drive.google.com/drive/folders/1jk-OkBFrw_mt4N547VpslrEyVJTv64za?usp=sharing",
+  },
+  {
+    name: "React course with Jonas Schmedtmann's in Udemy",
+    date: "2024",
+    site: "https://github.com/YasminMuntaserN/react-course.git",
+  },
+];
