b2dbfe632433e56b236ad5c5910f13de88b50892
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-11-11 15:34:35 +0200

    install react icons and scroll

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+// import eslintPlugin from "vite-plugin-eslint";
 
-// https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), eslintPlugin()],
+//   optimizeDeps: {
+//     include: ["@tanstack/react-query-devtools"],
+//   },
+// });
+
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    include: ["@tanstack/react-query-devtools"],
+  },
+});
