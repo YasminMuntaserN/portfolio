928d319f6e75e6cedc84cc753b5a6299fa31aedb
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-11-11 15:41:35 +0200

    refactor : edit Bubbles by adding the project name in the header

--- src/components/ui/Bubbles.jsx
+++ src/components/ui/Bubbles.jsx
@@ -1,10 +1,29 @@
-function Bubbles() {
+import styled from 'styled-components';
+import ProjectName from '../progicts/projectName';
+const StyledBubbles=styled.div`
+display: flex;
+gap:70%;
+border-bottom: 2px solid var(--color-border);
+padding: 10px ;
+position: relative;
+`;
+const BubbleSpan=styled.span`
+margin: 5px;
+background-color: ${prop =>prop.color};
+border-radius: 50%;
+padding: 0px 5px 0px 10px;
+`;
+
+function Bubbles({project}) {
   return (
-    <div className="bubbles">
-    <span className="bg-purple"></span>
-    <span className="bg-purple"></span>
-    <span className="bg-purple"></span>
-  </div>
+    <StyledBubbles>
+    <div>
+    <BubbleSpan color={'var( --main-color)'}></BubbleSpan>
+    <BubbleSpan color={'var( --second-color)'}></BubbleSpan>
+    <BubbleSpan color={'var(  --color-orang)'}></BubbleSpan>
+    </div>
+    <ProjectName project={project}/>
+  </StyledBubbles>
   )
 }
 
