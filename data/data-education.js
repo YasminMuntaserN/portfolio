4ca7d585a0ee104c967ed5a44abd465b5831e177
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-11-12 18:31:28 +0200

    refactor : edit the education data

--- data/data-education.js
+++ data/data-education.js
@@ -1,17 +1,25 @@
-export const education = [
+import { FaUserGraduate, FaProductHunt } from "react-icons/fa";
+import { SiUdemy } from "react-icons/si";
+export const educations = [
   {
     name: "University of Palestine",
-    date: "2022-present",
+    icon: FaUserGraduate,
+    date: "2022 - present",
+    siteName: "University of Palestine",
     site: "https://en.up.edu.ps/",
   },
   {
     name: "25 course with programming advice",
-    date: "2023-present",
+    icon: FaProductHunt,
+    date: "August 2023 - present",
+    siteName: "programming advice",
     site: "https://drive.google.com/drive/folders/1jk-OkBFrw_mt4N547VpslrEyVJTv64za?usp=sharing",
   },
   {
     name: "React course with Jonas Schmedtmann's in Udemy",
-    date: "2024",
+    icon: SiUdemy,
+    date: "September - November 2024",
+    siteName: "React course",
     site: "https://github.com/YasminMuntaserN/react-course.git",
   },
 ];
