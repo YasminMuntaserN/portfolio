357146746b4f51d19b5b0c0f6cf86a4ce96ffe16
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-11-13 19:33:08 +0200

    refactor: edit projects path

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import AboutMe from "./components/about/AboutMe";
 import Educations from "./components/educations/Educations";
 import Header from "./components/ui/Header";
-import Projects from "./components/progicts/projects";
 import Skills from "./components/skills/Skills";
 import StarterSection from "./components/home/StarterSection";
 import Modal from "./components/ui/Modal";
+import Projects from "./components/progicts/Projects";
 
 function App() {
   return (
